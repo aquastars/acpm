5c4b0a6e7c292463ded31bf2d94f2461c6a203a6
frankster <frank@aquastars.de>
2012-04-20 15:55:35 +0200

    new favActionsChart

---
+++ src/modules/favoriteActionsChart.js
@@ -0,0 +1,67 @@
+// modules/favoriteActionsChart.js
+// Module reference argument, assigned at the bottom
+;(function(ChartModule) {
+	 console.info("favoriteActionsChart module loaded:::");
+	 ChartModule.Model = Backbone.Model.extend({
+	   defaults: {
+	     action:"unknown_action",
+	     name:"unknown action",
+	     requests:600,
+	   }
+	 });
+	 
+	 ChartModule.List = Backbone.Collection.extend({
+	   model: ChartModule.Model,
+	   //order by 'value' key descending
+	   comparator:function(m) { return -m.get('value'); },
+	   initialize: function(query) {
+        	this.query = query||"cats";
+    	},
+
+    	url: function() {
+        	return "http://search.twitter.com/search.json?q=" + this.query + "&callback=?";
+    	},
+
+    	parse: function(data) {
+        	console.log("favoriteActionsChart DATA", data);
+        	// note that the original result contains tweets inside of a 'results' array, not at 
+        	// the root of the response.
+        			var arr = [];
+        			var actions=["set-carline","remove-package",'set-wheel','next=interior-page','set-extcolor'];
+        			actions=_.shuffle();
+		//mock/generate 1000 Items
+		for(var i = 0; i < 10; i++) {
+			arr.push(new ChartModule.Model({
+				action : actions[i],
+				name : "action_" + i,
+				requests : acpm.utils.getRandomNumberInRange(0, 500)
+			}));
+		}
+        	return arr
+    	}
+	 });
+
+	 ChartModule.Views.FavoriteActionsChart=Backbone.View.extend({
+		  		el: '#top5Chart',
+		  		initialize:function(){
+		  			this.tmpl=_.template($('#favoriteActionsChart-template').html()),
+		  			_.bindAll(this,"render");
+		  			this.collection.bind("reset",this.render);
+		  			console.info("FavoriteActionsChart initialized");
+		  		},
+		  		render: function() {
+		  			var coll=this.collection,max,i,item,len;
+		  			len=coll.length<6?coll.length:5;
+		  			//get max Value from collection
+		  			max=this.collection.max(function(co){ return co.get("value");});
+		  			log("max value in collection: "+max.get("value"),max);
+		  			max=max.get("value");
+		  			for(i=0;i<len;i++){
+		  				item=coll.at(i);
+		  				$(this.tmpl(item.toJSON())).appendTo(this.$("ul")).find(".chart-bar").css({width:0}).animate({width:(item.get('value')/max*this.$("ul").width())+"px"},1500);
+		  			};
+		    			return this;
+		  			}
+			});
+})(acpm.app.module("favoriteActionsChart"));
+
