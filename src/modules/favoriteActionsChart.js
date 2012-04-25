63e0a008c46432d790b7bb3443875fa39324d910
frankster <frank@aquastars.de>
2012-04-25 14:41:12 +0200

    mock modifications

--- src/modules/favoriteActionsChart.js
+++ src/modules/favoriteActionsChart.js
@@ -1,72 +1,68 @@
 // modules/favoriteActionsChart.js
 // Module reference argument, assigned at the bottom
 ;(function(ChartModule) {
-	 console.info("favoriteActionsChart module loaded:::");
-	 ChartModule.Model = Backbone.Model.extend({
-	   defaults: {
-	     action:"unknown_action",
-	     name:"unknown action",
-	     requests:600,
-	   }
-	 });
-	 
-	 ChartModule.List = Backbone.Collection.extend({
-	   model: ChartModule.Model,
-	   //order by 'value' key descending
-	   comparator:function(m) { return -m.get('requests'); },
-	   initialize: function(query) {
-        	this.query = query||"cats";
-    	},
-
-     url: function() {
-      if(acpm.app.isMocked){
-          return "http://search.twitter.com/search.json?q=" + this.query + "&callback=?";
-      }
-      else{
-        return //TODO ACPM URL
-      }
-      },
-    	parse: function(data) {
-        	console.log("favoriteActionsChart DATA", data);
-        	// note that the original result contains tweets inside of a 'results' array, not at 
-        	// the root of the response.
-        			var arr = [];
-        			var actions=["set-carline","remove-package",'set-wheel','next=interior-page','set-extcolor'];
-        			actions=_.shuffle(actions);
-		//mock/generate 1000 Items
-		for(var i = 0; i < 5; i++) {
-			arr.push(new ChartModule.Model({
-				action : actions[i],
-				name : "action_" + i,
-				requests : acpm.utils.getRandomNumberInRange(0, 5000)
-			}));
+	console.info("favoriteActionsChart module loaded:::");
+	ChartModule.Model = Backbone.Model.extend({
+		defaults : {
+			action : "unknown_action",
+			name : "unknown action",
+			requests : 600,
 		}
-        	return arr;
-    	}
-	 });
+	});
 
-	 ChartModule.Views.FavoriteActionsChart=Backbone.View.extend({
-		  		el: '#favoriteActionsChart',
-		  		initialize:function(){
-		  			this.tmpl=_.template($('#favoriteActionsChart-template').html()),
-		  			_.bindAll(this,"render");
-		  			this.collection.bind("reset",this.render);
-		  			console.info("FavoriteActionsChart initialized");
-		  		},
-		  		render: function() {
-		  			
-		  			var coll=this.collection,max,i,item,len;
-		  			len=coll.length<6?coll.length:5;
-		  			//get max Value from collection
-		  			max=this.collection.max(function(co){ return co.get("requests");});
-		  			log("max value in collection: "+max.get("requests"),max);
-		  			max=max.get("requests");
-		  			for(i=0;i<len;i++){
-		  				item=coll.at(i);
-		  				$(this.tmpl(item.toJSON())).appendTo(this.$("ul")).find(".chart-bar").css({width:0}).animate({width:(item.get('requests')/max*this.$("ul").width())+"px"},1500);
-		  			};
-		    			return this;
-		  			}
+	ChartModule.List = Backbone.Collection.extend({
+		model : ChartModule.Model,
+		//order by 'value' key descending
+		comparator : function(m) {
+			return -m.get('requests');
+		},
+		initialize : function(query) {
+			this.query = query || "cats";
+		},
+		url : function() {
+			if(acpm.app.isMocked) {
+				return "http://search.twitter.com/search.json?q=" + this.query + "&callback=?";
+			} else {
+				return //TODO ACPM URL
+			}
+		},
+		parse : function(data) {
+			console.log("favoriteActionsChart DATA", data);
+			// note that the original result contains tweets inside of a 'results' array, not at
+			// the root of the response.
+			var result = data;
+			if(acpm.app.isMocked) {
+				result = acpm.utils.mock.favoriteActionsData();
+			}
+			return result;
+		}
+	});
+
+	ChartModule.Views.FavoriteActionsChart = Backbone.View.extend({
+		el : '#favoriteActionsChart',
+		initialize : function() {this.tmpl = _.template($('#favoriteActionsChart-template').html()), _.bindAll(this, "render");
+			this.collection.bind("reset", this.render);
+			console.info("FavoriteActionsChart initialized");
+		},
+		render : function() {
+
+			var coll = this.collection, max, i, item, len;
+			len = coll.length < 6 ? coll.length : 5;
+			//get max Value from collection
+			max = this.collection.max(function(co) {
+				return co.get("requests");
 			});
+			log("max value in collection: " + max.get("requests"), max);
+			max = max.get("requests");
+			for( i = 0; i < len; i++) {
+				item = coll.at(i);
+				$(this.tmpl(item.toJSON())).appendTo(this.$("ul")).find(".chart-bar").css({
+					width : 0
+				}).animate({
+					width : (item.get('requests') / max * this.$("ul").width()) + "px"
+				}, 1500);
+			};
+			return this;
+		}
+	});
 })(acpm.app.module("favoriteActionsChart"));
-
--- src/modules/mockDataFactory.js
+++ src/modules/mockDataFactory.js
@@ -1,66 +1,108 @@
 // Module reference argument, assigned at the bottom
 ;(function(Mock) {
-	console.log("Mock initialized",Mock);
-	var carlines=["Q8","A4","A1","A6","A7","A3","A5","Q7","Q5","A8","A2"];
+	console.log("Mock initialized", Mock);
+	var carlines = ["Q8", "A4", "A1", "A6", "A7", "A3", "A5", "Q7", "Q5", "A8", "A2"];
+	var actions = ["set-carline", "remove-package", 'set-wheel', 'next=interior-page', 'set-extcolor'];
+	var pages = ["summary-page", "accessory-page", 'interior-page', 'carline-page', 'model-page'];
+	var mandanten = ["ACCx-3d-fr", "ACCx-3d-de","ACCx-fr","ACCx-de","ACCx-uk","ACCx-cn","other"];
 	//configured Carlines chart1
-	Mock.configuredCarlinesData=function(numVals,maxVal,minVal){
-		var i,j,arr,data;
-		
+	Mock.configuredCarlinesData = function(numVals, maxVal, minVal) {
+		var i, j, arr, data;
+
 		//
-		numVals=Math.min(numVals||5,carlines.length);
-		arr=[];
-			for(i=0;i<2;i++){
-				data=[];
-				for(j=0;j<numVals;j++){
-					data.push(acpm.utils.getRandomNumberInRange(minVal,maxVal));
-				}
-				arr[i]=data;
+		numVals = Math.min(numVals || 5, carlines.length);
+		arr = [];
+		for( i = 0; i < 2; i++) {
+			data = [];
+			for( j = 0; j < numVals; j++) {
+				data.push(acpm.utils.getRandomNumberInRange(minVal, maxVal));
 			}
-			var model={labels:carlines.slice(0,numVals),data:arr};
+			arr[i] = data;
+		}
+		var model = {
+			labels : carlines.slice(0, numVals),
+			data : arr
+		};
 		return model;
-	}	
+	}
 	//all requests chart
-	Mock.allRequestsData=function(min, max){
-		var data={
+	Mock.allRequestsData = function(min, max) {
+		var data = {
 			date : new Date().toString('yyyy-MM-dd'),
-			overallRequests : acpm.utils.getRandomNumberInRange(min||10000,max||75000),
-			tendency : "+"};
-			return data
+			overallRequests : acpm.utils.getRandomNumberInRange(min || 10000, max || 75000),
+			tendency : "+"
+		};
+		return data
 	}
 	//TOP5 Chart
-	Mock.top5Data=function(){
-		var Model,data = [];
-		Model=acpm.app.module("top5Chart").Model;
-			//mock/generate 1000 Items
-			for(var i = 0; i < 10; i++) {
-				data.push(
-					new Model({
-					id : i,
-					name : "Retailer_" + i,
-					value : acpm.utils.getRandomNumberInRange(0, 500)
-				}));
-			}
-		return data;
-	}
-	
-	Mock.minMaxRequestsData=function(){
-		var weekDays=["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"];
-		var data={
-				maxDay : weekDays[new Date(Math.round(Math.random() * 10000)).getDay()],
-				maxRequests : Math.round(Math.random() * 10000),
-				minDay : weekDays[new Date(Math.round(Math.random() * 100000)).getDay()],
-				minRequests : Math.round(Math.random() * 50000)
-				}
-		return data;
-	}
-	
-	Mock.mostConfiguredCarlineData=function(){
-		var data={
-			carline:carlines[acpm.utils.getRandomNumberInRange(0,carlines.length-1)],
-			configurations:acpm.utils.getRandomNumberInRange(0,10000)
+	Mock.top5Data = function() {
+		var Model, data = [];
+		Model = acpm.app.module("top5Chart").Model;
+		//mock/generate 1000 Items
+		for(var i = 0; i < 5; i++) {
+			data.push(new Model({
+				id : i,
+				name : "Retailer_" + i,
+				value : acpm.utils.getRandomNumberInRange(0, 500)
+			}));
 		}
 		return data;
-		
+	}
+
+	Mock.minMaxRequestsData = function() {
+		var weekDays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+		var data = {
+			maxDay : weekDays[new Date(Math.round(Math.random() * 10000)).getDay()],
+			maxRequests : Math.round(Math.random() * 10000),
+			minDay : weekDays[new Date(Math.round(Math.random() * 100000)).getDay()],
+			minRequests : Math.round(Math.random() * 50000)
+		}
+		return data;
+	}
+
+	Mock.mostConfiguredCarlineData = function() {
+		var data = {
+			carline : carlines[acpm.utils.getRandomNumberInRange(0, carlines.length - 1)],
+			configurations : acpm.utils.getRandomNumberInRange(0, 10000)
+		}
+		return data;
+	}
+	
+	Mock.sessionsPerMantantData=function(){
+		var Model,arr=[];
+		Model = acpm.app.module("sessionsPerMantantChart").Model;
+		for(var i=0;i<mandanten.length;i++){
+			arr.push(new Model({
+				mandant:mandanten[i],
+				sessions:acpm.utils.getRandomNumberInRange(333, 5000)
+			}));
+		};
+		return arr;
+	}
+	
+	Mock.favoriteActionsData=function(month){
+		var Model,arr=[];
+		Model = acpm.app.module("favoriteActionsChart").Model;
+		actions = _.shuffle(actions);
+		for(var i = 0; i < 5; i++) {
+				arr.push(new Model({
+					action : actions[i],
+					name : "action_" + i,
+					requests : acpm.utils.getRandomNumberInRange(0, 5000)
+				}));
+			}
+		return arr;
+	}
+	
+	Mock.pageRequestsData=function(month){
+		var Model,arr=[];
+		Model = acpm.app.module("pageRequestsChart").Model;
+		for(var i=0;i<pages.length;i++){
+			arr.push(new Model({
+				page:pages[i],
+				requests:acpm.utils.getRandomNumberInRange(333, 5000)
+			}));
+		};
+		return arr;
 	}
 })(acpm.namespace("acpm.utils.mock"));
-
