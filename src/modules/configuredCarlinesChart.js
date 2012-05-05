d54e824102333e3f9b58f43025e49ddbd5b83092
Frank Schiemann <frank@aquastars.de>
2012-05-05 22:49:37 +0200

    dropdowns added

--- src/modules/configuredCarlinesChart.js
+++ src/modules/configuredCarlinesChart.js
@@ -7,6 +7,7 @@
 		defaults : {
 			labels : ["Q8", "A4", "A1", "A6", "A7"],
 			data : [[17, 20, 75, 48, 32], [38, 51, 49, 38, 59]],
+			dates:["01/2012","02/2012","03/2012","04/2012","05/2012"]
 		},
 		url : function() {
 			if(acpm.app.isMocked) {
@@ -26,6 +27,20 @@
 
 	ChartModule.Views.ConfiguredCarlinesChart = Backbone.View.extend({
 		el : '#configuredCarlinesChart',
+		events:{
+			"change .upperSelect":"onDateSelect",
+			"change .lowerSelect":"onDateSelect"
+		},
+		"selectedValues":null,
+		onDateSelect:function(evt){
+			if(evt.currentTarget.className==="upperSelect"){
+				this.selectedValues[0]=evt.currentTarget.value;
+			}
+			else{
+				this.selectedValues[1]=evt.currentTarget.value;
+			}
+			this.model.fetch();
+		},
 		initialize : function() {
 			console.info("ConfiguredCarlinesChart initialized");
 			_.bindAll(this, "render");
@@ -33,6 +48,11 @@
 		},
 		render : function() {
 			console.log("MODEL", this.model);
+			this.initDropDownMenu();
+			this.drawChart();
+			return this;
+		},
+		drawChart:function(){
 			var canvas = this.$(".acpm-chart ").empty()[0];
 			var chart = new acpm.modules.charts.DoubleBarChart(canvas, {
 				width : canvas.width,
@@ -41,8 +61,22 @@
 				labels : this.model.get("labels")
 			});
 			chart.draw();
-			return this;
+		},
+		initDropDownMenu:function(){
+			//no dropdown was selected=>select first and second
+			if(!this.selectedValues){
+				var len=this.model.get("data").length;
+				for(var i=0;i<len;i++){
+					this.$(".upperSelect").append("<option>"+this.model.get("dates")[i]+"</option>");
+					this.$(".lowerSelect").append("<option>"+this.model.get("dates")[i]+"</option>");
+				}
+				this.$(".upperSelect").selectedIndex=0;
+				this.$(".lowerSelect").selectedIndex=1;
+				this.selectedValues=[this.model.get("dates")[0],this.model.get("dates")[1]];
+			}
 		}
+
+
 	});
 
 })(acpm.app.module("configuredCarlinesChart"));
